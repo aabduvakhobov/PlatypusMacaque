7d730e39e74319d5a36eaa1eab3c4c83f27dbb15
aabduvakhobov <aaabduv@gmail.com>
2025-08-25 05:20:29 +0200

    Fix minor issues in ablation experiment

--- Experiments/Ablation-Experiment/ablation_experiment.sh
+++ Experiments/Ablation-Experiment/ablation_experiment.sh
@@ -2,17 +2,19 @@
 
 set -e
 
-ModelarDB_vanilla_path="../../ModelarDB-versions/ModelarDB-GorillaV/"
+ModelarDB_vanilla_path="../../ModelarDB-versions/ModelarDB/"
 # Where ModelarDB stores data
-ModelarDB_Data="../../ModelarDB-versions/data/"
+ModelarDB_Data="/srv/data4/abduvoris/modelardb_data/"
 
 # TODO: confirm that path to ModelarDB Utilities is correct
-ingestion_script="~/Utilities/Apache-Parquet-Loader/main.py"
+ingestion_script="../../Utilities/ingest_parquet_to_modelardb.py"
 output_dir=$(pwd)
 
 table_name=$1
 data_dir=$2
+
 error_bounds="0 0.01 0.1 1 5 10"
+#error_bounds="0 0.01 0.1"
 # ModelarDB's default port number
 port="127.0.0.1:9999"
 # time to sleep for vacuum
@@ -52,32 +54,31 @@ stop_modelardb() {
 }
 
 compress_error_bounds() {
-    for patch in $pmc_only_patch $pmc_and_swing_only_patch $swing_only_patch $gorilla_only_patch; do
-        cd $ModelarDB_vanilla_path
-        git restore .
-        git apply $patch
+    for patch in $pmc_only_patch $swing_only_patch $pmc_and_swing_only_path $gorilla_only_patch; do
+        #git restore .
+        #git apply $patch
         patch_name=$(basename -s .patch "$patch")
         echo "Patch name is: $patch_name"
         for error_bound in $error_bounds; do
             # Ensure release build is done
-            # cargo build --release --manifest-path $1/Cargo.toml
-            # start_modelardb $1/target/release/modelardbd
+            cargo build --release --manifest-path $1/Cargo.toml
+            start_modelardb $1/target/release/modelardbd
             results_file=$patch_name-$error_bound-$table_name-compression_results.log
             # timing ingestion process
             start=$SECONDS
-            # python3 $ingestion_script "127.0.0.1:9999" $table_name $data_dir $error_bound
+            python3 $ingestion_script "127.0.0.1:9999" $table_name $data_dir $error_bound
             # sleep for a minute or two to finish vacuuming
             duration=$((SECONDS-start))
             # write results of the python program logs to the common file
-            # sleep $sleep_for_vacuum
+            sleep $sleep_for_vacuum
             echo "Compressed in $duration seconds" > $results_file
             compression_size=$(du -h -d0 $ModelarDB_Data)
             echo "Compression size: $compression_size" >> $results_file
             # stop ModelarDB
-            # stop_modelardb
-            # sleep $sleep_for_vacuum
+            stop_modelardb
+            sleep $sleep_for_vacuum
             # We remove old data after each ingestion
-            # clean_modelar_data
+            clean_modelar_data
         done
     done
 }
@@ -87,6 +88,7 @@ compress_error_bounds() {
 # Main function
 clean_modelar_data
 # start new round with modified modelardb
+cd $ModelarDB_vanilla_path
 for path in $ModelarDB_vanilla_path; do
     echo "Starting: $path"
     # compress_error_bounds $path
